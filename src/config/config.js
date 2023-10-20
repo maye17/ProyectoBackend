b7f4e95bb50a61157119423faa69155d1dd56c34
maye17 <mayerlin.dossantos@gmail.com>
2023-10-20 16:12:28 -0300

    modificando ruta de datos de session

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-program.option('--mode <mode>','Modo de trabajo', 'DEVELOPMENT');
+program.option('--mode <mode>','Modo de trabajo', 'PRODUCTION');
 //program.option("-d", "Variables para debug", false)
 program.parse();
 
